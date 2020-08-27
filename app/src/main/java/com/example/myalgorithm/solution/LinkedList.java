87ede903dae407c92c9720d7c247732686eb5e12
luozedong <873821975@qq.com>
2020-08-27 09:38:15 +0800

    LinkedList test.

--- app/src/main/java/com/example/myalgorithm/solution/LinkedList.java
+++ app/src/main/java/com/example/myalgorithm/solution/LinkedList.java
@@ -1,23 +1,42 @@
 package com.example.myalgorithm.solution;
+
 //链表题
-//1.链表表尾为 null;
+//1.单链表表尾为 null;
 //2.大部分题可以使用预先节点，现节点,临时节点和下一节点
 // ListNode prev;
 // ListNode curr;
 // ListNode temp;
 // ListNode next;
+// ListNode dummy; //虚拟节点
+//3.list.next 为指向作用
+// list.next = list.next.next; //跳过下一节点，即删除某节点，Java具有垃圾回收机制
 public class LinkedList {
     public LinkedList() {
 
     }
-
-    //q2两链表值之和
-    public ListNode addTwoNumber(ListNode list1, ListNode list2) {
+    public static ListNode createLinkedList(int size) {
+        ListNode linkedList = new ListNode(0);
+        ListNode temp = linkedList;
+        for (int i = 1; i < size; i++) {
+            temp.next = new ListNode(i);
+            temp = temp.next;
+        }
+        temp.next = null;
+        ListNode printList = temp;
+//        System.out.println("Create linkedList:");
+//        while (printList != null) {
+//            System.out.println(printList);
+//            printList = printList.next;
+//        }
+        return linkedList;
+    }
+    //q2 两链表值之和
+    public static ListNode addTwoNumber(ListNode list1, ListNode list2) {
         ListNode result = new ListNode(list1.val + list2.val);
         return null;
     }
 
-    //q206反转链表
+    //q206 反转链表
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
@@ -30,22 +49,56 @@ public class LinkedList {
         return prev;
     }
 
-    public static ListNode createLinkedList(int size) {
-        ListNode linkedList = new ListNode(0);
-        for(int i = 0; i < size; i++) {
-            linkedList.next = new ListNode(i);
+    //q19 删除倒数第N个节点
+    //两次遍历 单指针
+    public static ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length = 0;
+        ListNode first = head;
+        while (first != null) {
+            length++;
+            first = first.next;
         }
-        return linkedList;
+        length -= n;
+        first = dummy;
+        while (first != null) {
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
     }
+    //一次遍历 双指针
+    public static ListNode removeNthFromEnd1(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for (int i = 0; i < n + 1; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+    //旋转链表
+
     public static void main(String[] args) {
 
         LinkedList linkedList = new LinkedList();
-        ListNode List = createLinkedList(4);
-        ListNode reverseList =  linkedList.reverseList(List);
+        ListNode list = createLinkedList(6);
         System.out.println("result:");
-        while (reverseList != null) {
-            System.out.println(reverseList.val);
-            reverseList= reverseList.next;
+        //反转链表
+//        ListNode resultList = reverseList(list);
+        ListNode resultList = removeNthFromEnd1(list, 2);
+        while (resultList != null) {
+            System.out.println(resultList.val);
+            resultList = resultList.next;
         }
     }
 }
